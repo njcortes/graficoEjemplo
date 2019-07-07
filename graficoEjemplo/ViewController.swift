b4dc0a7ae22588b9bd6f7700705e0060de11c534
Nestor <nj0588@gmail.com>
2019-07-07 00:01:09 -0400

    carga inicial

--- graficoEjemplo/ViewController.swift
+++ graficoEjemplo/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chartLinea: LineChartView!
+    @IBOutlet weak var chartBarraHorizontal: HorizontalBarChartView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        generaGraficoBarra()
+        generaGraficoLinea()
     }
 
-
+    func generaGraficoBarra () {
+        let dato1 = BarChartDataEntry(x: 1.0, y: Double(7))
+        let dato2 = BarChartDataEntry(x: 2.0, y: Double(6))
+        let dato3 = BarChartDataEntry(x: 3.0, y: Double(3))
+        
+        let dataSet = BarChartDataSet(entries: [dato1, dato2, dato3], label: "Leyenda Barra")
+        let data = BarChartData(dataSets: [dataSet])
+        chartBarraHorizontal.data = data
+        chartBarraHorizontal.notifyDataSetChanged()
+    }
+    
+    func generaGraficoLinea () {
+        let dato1 = BarChartDataEntry(x: 1.0, y: Double(7))
+        let dato2 = BarChartDataEntry(x: 2.0, y: Double(6))
+        let dato3 = BarChartDataEntry(x: 3.0, y: Double(3))
+        let dataSet = LineChartDataSet(entries: [dato1, dato2, dato3], label: "Leyenda Linea")
+        let data = LineChartData(dataSets: [dataSet])
+        chartLinea.data = data
+        chartLinea.notifyDataSetChanged()
+    }
 }
 
